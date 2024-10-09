539f691190e1ad88e1ff8ba126fe142dd11d2aca
aayushjoshi244 <aayush09345@gmail.com>
2024-10-09 17:53:20 +0530

    another random commit day

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,48 @@
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+import Socials from "@/components/Socials";
+import Photo from "@/components/Photo";
+
 
 const page = () => {
   return (
-    <div>
-      HomePage
-    </div>
-  )
-}
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Software Developer</span>
+            <h1 className="h1">
+              Hello I'm <br /> <span className="text-accent">Aayush Joshi</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              {" "}
+              I excel at creating elegant digital experience and I'm proficient
+              in various programing languages and technologies.
+            </p>
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button
+                variant="outline"
+                size="lg"
+                className="uppercase f;ex items-center gap-2"
+              >
+                <span>Download CV</span>
+                <FiDownload className="text-xl" />
+              </Button>
+              <div className="mb-8 xl:mb-0">
+                <Socials
+                  containerStyles="flex gap-6"
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
+                />
+              </div>
+            </div>
+          </div>
+          <div>
+            <Photo />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default page
+export default page;
